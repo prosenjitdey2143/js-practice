41c20bddd0a464f6dfb6fd237cd83d55686f8d6e
prosenjitdey2143 <136630827+prosenjitdey2143@users.noreply.github.com>
2023-10-27 19:54:51 +0000

    datatypes

--- day1/test.js
+++ day1/test.js
@@ -1,1 +1,8 @@
 console.log("hello world");
+const userPassword="gitadey143"
+let userName="gita dey"
+let userId=22312
+let temp=null
+console.table([userPassword,userId,userName])
+console.log(typeof null) //null ka datatype hai object
+console.log(typeof undefined) //undefined ka datatype hai undefined
