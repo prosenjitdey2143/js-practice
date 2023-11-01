944cc117cc086eed0297d78cfb158a6f32deecc2
prosenjitdey2143 <136630827+prosenjitdey2143@users.noreply.github.com>
2023-11-01 20:52:12 +0000

    project5 done

---
+++ project/project5/script.js
@@ -0,0 +1,31 @@
+
+let randomColor=function(){
+    let hex='012345678abcdef'
+    let color="#"
+    for(let i=0;i<6;i++){
+        color+=hex[Math.floor(Math.random()*16)]
+    }
+    return color;
+}//for hex code
+let createRGB=function(){
+    let r=Math.floor(Math.random()*255)
+    let g=Math.floor(Math.random()*255)
+    let b=Math.floor(Math.random()*255)
+    let a=Math.random().toFixed(1)
+    let color=`rgba(${r},${g},${b},${a})`
+    return color
+} //for rgba code
+console.log(randomColor())
+let intervelId;
+let startChangeColor=function(){
+    function changeBackgroundColor(){
+        document.querySelector('.main').style.backgroundColor=createRGB()
+    }
+    intervelId=setInterval(changeBackgroundColor,1000)
+    
+}
+let stopChangeColor=function(){
+    clearInterval(intervelId)
+}
+document.querySelector('#start').addEventListener('click',startChangeColor)
+document.querySelector('#end').addEventListener('click',stopChangeColor)
